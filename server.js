7cc9ad49799cf0a246e4ae6acbbfb2f83dacab9b
Ak28shaya <abiakshaya206@gmail.com>
2026-06-29 23:22:42 +0530

    changes in node modules

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const UserPasskey = require("./model/userpasskey");
 const Contact = require("./schema");
+
 
 const app = express();
 app.use(cors({
